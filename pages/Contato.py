4b2d364d8ffc07099c448f3cc0c12cca3c69e759
RodrigoOCS <rodigocelular@gmail.com>
2022-10-08 20:40:59 -0300

    contatos

---
+++ pages/Contato.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+st.header("Contato")
+st.write('Me envie uma mensagem!')
+contact_form = """
+<form action="https://formsubmit.co/bot.crescento@gmail.COM" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+local_css("style/style.css")
