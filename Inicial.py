b092e43d72d8ac3583fb4049d3a1f8353cbd9f73
RodrigoOCS <rodigocelular@gmail.com>
2022-10-09 17:23:45 -0300

    mais bunitin

--- Inicial.py
+++ Inicial.py
@@ -9,7 +9,10 @@ st.set_page_config(page_title="Inicial", page_icon="👋", layout="centered")
 st.header('Página inicial')
 
 st.write('Olá!')
-st.write('Esse é meu aplicativo de acompanhamento financeiro')
+st.write('Esse é meu aplicativo de modelagem de dados financeiros.')
+st.write('Ele auxlia no processo de importar, modelar e salvar os dados.')
+st.write('E principalmente - automatiza o processo de programar os modelos!')
+
 
 lt_path = Path(__file__).parent / "pages/utils/lottiev1.json"
 with open(lt_path, "r") as f:
