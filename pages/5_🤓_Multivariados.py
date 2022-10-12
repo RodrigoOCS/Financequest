310c856c648c031ff0901e1f5fd86e282bdb9ed1
RodrigoOCS <rodigocelular@gmail.com>
2022-10-12 19:17:25 -0300

    mais bunitin

--- pages/5_🤓_Multivariados.py
+++ pages/5_🤓_Multivariados.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Modelos Multivariados", page_icon="🤓", layout="centered")
 st.write('Em desenvolvimento 🏗️')
 
+
 import yfinance as yf
 import pandas as pd
 # Dowload data
