b35a7615fd63accf8d32e98c5a8f82c4b85b30ca
RodrigoOCS <rodigocelular@gmail.com>
2022-10-15 21:10:45 -0300

    logo

--- Inicial.py
+++ Inicial.py
@@ -2,8 +2,17 @@ import streamlit as st
 from streamlit_lottie import st_lottie  # pip install streamlit-lottie #lotties='https://lottiefiles.com/'
 import json
 from pathlib import Path
+import base64
+
+file_ = open(Path(__file__).parent/"ROCS.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+
 
 st.set_page_config(page_title="Inicial", page_icon="👋", layout="centered")
+st.sidebar.markdown(f'<img src="data:image/gif;base64,{data_url}" width="100" height="100" alt="cat gif">',unsafe_allow_html=True)
+
 
 st.header('Página inicial')
 
